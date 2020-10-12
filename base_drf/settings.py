44e7724f6d0be606ebda08d2e545f0764926e552
VuNT <you@example.com>
2020-10-12 16:21:57 +0700

    update env

--- base_drf/settings.py
+++ base_drf/settings.py
@@ -16,7 +16,7 @@ from decouple import Config, RepositoryEnv, config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-ENV_FILE = os.path.join(BASE_DIR, '.env.dev')
+ENV_FILE = os.path.join(BASE_DIR, '.env')
 env_config = Config(RepositoryEnv(ENV_FILE))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
